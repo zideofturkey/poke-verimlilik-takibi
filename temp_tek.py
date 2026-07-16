aabfbce8e0939d292a5fb91db923fe184c1475e4
Ediz Kaçmaz <ediz@example.com>
2026-07-16 01:08:08 +0000

    Gecici: satir 10 sil

--- temp_tek.py
+++ temp_tek.py
@@ -1,6 +1,6 @@
 from common import get_haftalik_sheet
 ws = get_haftalik_sheet()
-ws.delete_rows(11)
+ws.delete_rows(10)
 with open("tek_sonuc.txt", "w") as f:
-    f.write("satir 11 silindi")
+    f.write("satir 10 silindi")
 print("bitti")
