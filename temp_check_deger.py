c787b787e0637615759a87b65150b376bdec4505
Poke Claude Session <ediz-poke@users.noreply.github.com>
2026-08-19 18:05:37 +0000

    gecici: son sans tarihi degerini kontrol et

---
+++ temp_check_deger.py
@@ -0,0 +1,2 @@
+from common import get_deger
+print("gecen_hafta_rutin_son_sans_tarihi:", repr(get_deger("gecen_hafta_rutin_son_sans_tarihi")))
--- temp_test_son_sans.py
+++
@@ -1,5 +0,0 @@
-# gonder.py'deki fonksiyonu hafta kontroluna takilmadan dogrudan cagirir -
-# mantigin kendisini test etmek icin (gercek Pazartesi kontrolu ayri, bu
-# zaten sabah()'in en basinda otomatik calisiyor).
-import gonder
-gonder._gecen_hafta_rutin_son_sans_ver()
